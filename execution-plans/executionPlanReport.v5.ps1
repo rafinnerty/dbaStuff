967cf3cb53947687bb840940d922371bc2c014bb
Richard Armstrong-Finnerty <richard.armstrong.finnerty@gmail.com>
2026-05-21 08:12:40 +0100

    Fix doubling of lines from end of line fix

--- execution-plans/executionPlanReport.v5.ps1
+++ execution-plans/executionPlanReport.v5.ps1
@@ -69,7 +69,7 @@ function Get-SqlPlanInsights {
     [switch]$DebugSargability
   )
 
-  Write-Output 'v3'
+  Write-Output 'v5'
 
   if ([string]::IsNullOrWhiteSpace($Path)) {
     throw "No plan file specified. Use -Path to supply a .sqlplan or .xml file."
